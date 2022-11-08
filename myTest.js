4641051918ed344a99989b666b5ebca2eb31389e
N-Martinez0916 <nicomartin0916@gmail.com>
2022-11-08 14:27:47 -0500

    removed myTest.js

--- myTest.js
+++
@@ -1,7 +0,0 @@
-const {getAllRoutinesByUser} = require('./db')
-
-try {
-    getAllRoutinesByUser({username: 'albert'}).then( result => console.log("**** test ****", result))  
-} catch (error) {
-    console.error(error)
-}
