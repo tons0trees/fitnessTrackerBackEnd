96840b27ddb36b704fc2fd12208dc5c2687c4b4a
tons0trees <forrest@fmcclure.com>
2022-11-09 11:46:10 -0700

    login working

--- api/users.js
+++ api/users.js
@@ -1,20 +1,37 @@
 const express = require('express');
-const { createUser, getUserByUsername } = require('../db');
+const { createUser, getUser, getUserByUsername } = require('../db');
 const router = express.Router();
 const jwt = require('jsonwebtoken')
 
 router.use((req, res, next) => {
     console.log("A request has been made to users...")
-
     next();
 })
 // POST /api/users/login
+router.post('/login', async (req, res, next) => {
+    const { username, password } = req.body;
+    const user = await getUser({ username, password })
+
+    if (user) {
+        const token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: '1w' })
+
+        res.send({
+            user,
+            token
+        })
+    } else {
+        next({
+            name: 'InvalidUsernameOrPassword',
+            message: 'The username or password provided was not valid'
+        })
+    }
+})
 
 // POST /api/users/register
 router.post('/register', async (req, res, next) => {
+    const { username, password } = req.body;
 
     try {
-        const { username, password } = req.body;
         const user = await getUserByUsername(username)
 
         if (user) {
@@ -22,16 +39,16 @@ router.post('/register', async (req, res, next) => {
                 name: 'UserExists',
                 message: 'This user is taken'
             });
+        } else {
+            const createdUser = await createUser({ username, password });
+    
+            const token = jwt.sign(createdUser, process.env.JWT_SECRET, { expiresIn: '1w' })
+    
+            res.send({
+                createdUser,
+                token
+            });
         }
-        const createdUser = await createUser({ username, password });
-        console.log('**** looky here ****', createdUser) // Still see this log when trying to register a duplicate user
-
-        const token = jwt.sign(createdUser, process.env.JWT_SECRET, { expiresIn: '1w' })
-
-        res.send({
-            createdUser,
-            token
-        });
     } catch ({ name, message }) {
         next({
             name,
